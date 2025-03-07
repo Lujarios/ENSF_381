7cbb786bf50a224afc9e063a50d549d954ceccc4
millainel <144377899+millainel@users.noreply.github.com.>
2025-03-07 11:35:31 -0700

    did exercise 2 lab 6

--- Labs/Lab_6/jsx-exercise/src/App.js
+++ Labs/Lab_6/jsx-exercise/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home.js'
+import About from './About.js'
+import Contact from './Contact.js'
+
 
 function App() {
   let currentDate = new Date();
@@ -12,6 +16,10 @@ function App() {
       <p>React Components</p>
       <p>{currentYear}</p>
       <p>{loggedIn(isLoggedIn)}</p>
+      <p>{Home("Home Page", "Welcome to our website.")}</p>
+      <p>{About("About Us", "We are passionate about delivering quality experiences.")}</p>
+      <p>{Contact("Contact Us", "Feel free to reach out to us via email or phone.")}</p>
+
     </div>
   );
 }
